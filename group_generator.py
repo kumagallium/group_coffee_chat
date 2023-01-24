ac7f308056de1f053b91f41ef0cd053efbedcf09
kumagallium <kumagai.110h@gmail.com>
2023-01-24 15:07:53 +0900

    fix: added the ability to output talk themes

--- group_generator.py
+++ group_generator.py
@@ -28,39 +28,57 @@ def main():
     members_num = len(members)
     group1_num = int((members_num - (members_num % 2)) / 2)
     group2_num = members_num - group1_num
-    all_idxs = list(range(len(members)))
-    group1 = random.sample(set(all_idxs), k=group1_num)
-    remain_idxs = set(all_idxs) ^ set(group1)
-    group2 = random.sample(remain_idxs, k=group2_num)
 
-    group1_name = [group1_mc]
-    for g in group1:
-        group1_name.append(members[g])
+    group1_tmp = random.sample(set(members), k=group1_num)
+    remains = set(members) ^ set(group1_tmp)
+    group2_tmp = random.sample(set(remains), k=group2_num)
 
-    group2_name = [group2_mc]
-    for g in group2:
-        group2_name.append(members[g])
+    group1 = [group1_mc] + group1_tmp
+    group2 = [group2_mc] + group2_tmp
 
-    group1_m = ",\n".join(group1_name)
+    group1_m = ",\n".join(group1)
+    group2_m = ",\n".join(group2)
 
-    group2_m = ",\n".join(group2_name)
+    with open('topics.txt') as f:
+        topics_tmp = f.read()
+    all_topics = topics_tmp.split("\n")
+    topics = random.sample(set(all_topics), k=5)
+    topics_m =  "\n*".join(topics)
+    topics_m =  "*" + topics_m
+
+
+    print(group1_m)
+    print(group2_m)
+    print(topics_m)
 
     attachments = [
         {
             # "fallback":"fallback",
             # "pretext":"",
             "color": "#06a790",
-            "fields": [{"title": "Group1", "value": group1_m}],
+            "fields": [{"title": "グループ1", "value": group1_m}],
         },
         {
             # "fallback":"fallback",
             # "pretext":"",
             "color": "#fdea8f",
-            "fields": [{"title": "Group2", "value": group2_m}],
-        },
+            "fields": [{"title": "グループ2", "value": group2_m}],
+        },{
+            "blocks": [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": "本日のトークテーマです！参考にしてください。\n```"+topics_m+"```"
+                    }
+                }
+            ]
+        }
     ]
     message = "本日のグループは、以下の通りです。"
     send_message_to_slack(message, attachments)
+
+
 
 
 if __name__ == "__main__":
