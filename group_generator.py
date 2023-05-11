3530a7635f6fe9505aec6934698bb0ba7832a618
kumagallium <kumagai.110h@gmail.com>
2023-05-11 12:25:36 +0900

    fix: fixed the number of divisions to be adjustable

--- group_generator.py
+++ group_generator.py
@@ -1,11 +1,20 @@
+import os
 import random
 import urllib.request
 import json
-import os
 
 
 def send_message_to_slack(msg, attachments):
-    # make slack message body
+    """
+    Send a message to Slack.
+
+    Parameters:
+    msg (str): The main message to be sent to the Slack channel.
+    attachments (list): The list of attachments to be included in the message.
+
+    Returns:
+    None
+    """
     send_data = {
         "channel": os.environ["SLACK_CHANNEL"],
         "attachments": attachments,
@@ -19,65 +28,71 @@ def send_message_to_slack(msg, attachments):
     with urllib.request.urlopen(slack_request) as slack_response:
         response_body = slack_response.read().decode("utf-8")
 
+
+def divide_members(num_people, num_groups):
+    """
+    Divide the members into the specified number of groups.
+
+    Parameters:
+    num_people (int): The number of people to be divided into groups.
+    num_groups (int): The number of groups to divide the people into.
+
+    Returns:
+    A list of the number of members in each group.
+    """
+    base_group_size = num_people // num_groups
+    group_sizes = [base_group_size] * num_groups
+    remainder = num_people % num_groups
+    for i in range(remainder):
+        group_sizes[i] += 1
+    return group_sizes
+
+
 def main():
+    # Load environment variables
     members = eval(os.environ["MEMBERS"])
-    group1_mc = members[0]
-    group2_mc = members[1]
-    members = members[2:]
+    slack_mentions = eval(os.environ["SLACK_MENTIONS"])
+    num_groups = int(os.environ["NUM_DIVISIONS"])
+    group_names = eval(os.environ["GROUP_NAMES"])
 
-    members_num = len(members)
-    group1_num = int((members_num - (members_num % 2)) / 2)
-    group2_num = members_num - group1_num
+    # Divide members into groups
+    group_sizes = divide_members(len(members), num_groups)
+    groups = []
+    for group_size in group_sizes:
+        group = random.sample(members, k=group_size)
+        groups.append(group)
+        members = [member for member in members if member not in group]
 
-    group1_tmp = random.sample(set(members), k=group1_num)
-    remains = set(members) ^ set(group1_tmp)
-    group2_tmp = random.sample(set(remains), k=group2_num)
-
-    group1 = [group1_mc] + group1_tmp
-    group2 = [group2_mc] + group2_tmp
-
-    group1_m = ",\n".join(group1)
-    group2_m = ",\n".join(group2)
-
-    with open('topics.txt') as f:
-        topics_tmp = f.read()
-    all_topics = topics_tmp.split("\n")
-    topics = random.sample(set(all_topics), k=5)
-    topics_m =  "\n*".join(topics)
-    topics_m =  "*" + topics_m
-
-    mentions = eval(os.environ["SLACK_MENTIONS"])
-    mention_m = "<"+"> <".join(mentions)+">"
-
-    attachments = [
-        {
-            # "fallback":"fallback",
-            # "pretext":"",
+    # Prepare message attachments for Slack
+    attachments = []
+    for i, group in enumerate(groups):
+        attachments.append({
             "color": "#06a790",
-            "fields": [{"title": "グループ1", "value": group1_m}],
-        },
-        {
-            # "fallback":"fallback",
-            # "pretext":"",
-            "color": "#fdea8f",
-            "fields": [{"title": "グループ2", "value": group2_m}],
-        },{
-            "blocks": [
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "本日のトークテーマです！（ご参考までに）\n```"+topics_m+"```"
-                    }
+            "fields": [{"title": group_names[i], "value": ",\n".join(group)}],
+        })
+
+    # Prepare message body for Slack
+    with open('topics.txt') as f:
+        all_topics = f.read().split("\n")
+    topics = random.sample(all_topics, k=5)
+    topics_message = "*\n".join(topics)
+
+    message = f"<{'> <'.join(slack_mentions)}> \n本日は、Coffee Chatの日です。\nグループは、以下の通りです。"
+    # Append topic message to attachments
+    attachments.append({
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": f"本日のトークテーマです！（ご参考までに）\n```{topics_message}```"
                 }
-            ]
-        }
-    ]
-    message = mention_m + "\n本日は、Coffee Chatの日です。\nグループは、以下の通りです。"
+            }
+        ]
+    })
+
+    # Send message to Slack
     send_message_to_slack(message, attachments)
-
-
-
 
 if __name__ == "__main__":
     main()
